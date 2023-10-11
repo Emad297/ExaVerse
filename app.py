7a92f07ed48fd7c0ba5665017eb24a967c5e9dda
Emad <emadolfatbakhsh@gmail.com>
2023-10-12 00:41:47 +0200

    revert flask session changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify, render_template, session
-from flask_session import Session
+from flask import Flask, request, jsonify, render_template
 import openai
 import os
 
@@ -7,10 +6,6 @@ app = Flask(__name__)
 
 # Initialize OpenAI API with your secret key from the Azure environment
 openai.api_key = os.environ.get('OPENAI_SECRET_KEY')
-# Configuration for the session
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
 
 @app.route('/')
 def index():
@@ -18,25 +13,13 @@ def index():
 
 # Generate text from prompt from gpt-3.5-turbo model
 @app.route('/generate', methods=['POST'])
-@app.route('/generate', methods=['POST'])
 def generate_text():
     user_message = request.json.get('prompt')
-    
-    # Get previous messages from session or initialize an empty list if not found
-    messages = session.get('messages', [])
-    messages.append({"role": "user", "content": user_message})
-    
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
+        model="gpt-3.5-turbo", 
+        messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible."},
+        {"role": "user", "content" : user_message}]
     )
-    
-    # Append the model's response to messages
-    messages.append({"role": "assistant", "content": completion.choices[0].message['content']})
-    
-    # Update the session with the modified messages
-    session['messages'] = messages
-    
     return jsonify({'text': completion.choices[0].message['content']})
 
 if __name__ == '__main__':
