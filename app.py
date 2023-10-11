14343f7fc2e618b979d076ea2fe04e65a3ace5b4
Emad <emadolfatbakhsh@gmail.com>
2023-10-11 23:45:14 +0200

    fix the gpt prompting

--- app.py
+++ app.py
@@ -13,9 +13,17 @@ def index():
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
-    prompt = request.json.get('prompt')
-    response = openai.Completion.create(model="gpt-3.5-turbo", prompt=prompt, max_tokens=100, temperature=0.7, top_p=1, frequency_penalty=0, presence_penalty=0.3, stop=["\n"])
-    return jsonify(response.choices[0].text.strip())
+    user_message = request.json.get('prompt')
+    
+    # Use conversation method
+    messages = [{"role": "user", "content": user_message}]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    response_message = response["choices"][0]["message"]["content"]
+    
+    return jsonify(response_message.strip())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
