0ef0a637d9aa7588d9250fb52d02984a52cd0afc
Emad <emadolfatbakhsh@gmail.com>
2023-10-11 22:05:59 +0200

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import openai
 import os
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 # Initialize OpenAI API with your secret key from the Azure environment
 openai.api_key = os.environ.get('OPENAI_SECRET_KEY')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
